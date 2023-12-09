b99fe97134a0986c2745d210697042897bdea61d
vishushar <117721545+vishushar@users.noreply.github.com>
2023-12-09 13:02:52 +0530

    Update Meeting.java

--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -10,5 +10,21 @@ public class Meeting {
         this.startTime = startTime;
         this.endTime = endTime;
     }
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+   // public void setStartTime(LocalTime startTime) {
+  //      this.startTime = startTime;
+  //  }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    //public void setEndTime(LocalTime endTime) {
+     //   this.endTime = endTime;
+    //}
+
 
 }
